dc917d6c5f7dacc3c6ce0c960a71c156b8eaa32e
Felipe Borges Ferreira <lypborges@gmail.com>
2016-06-30 16:42:59 -0300

    add first call

--- lib/pipedrive_ruby.rb
+++ lib/pipedrive_ruby.rb
@@ -1,7 +1,7 @@
 require "pipedrive_ruby/version"
 require "http"
-
-require "resources/Deals"
+require "resources/base"
+require "resources/deals"
 
 # If you need to use the PipedriveAPI
 # https://developers.pipedrive.com/v1
--- lib/resources/deals.rb
+++ lib/resources/deals.rb
@@ -2,11 +2,11 @@
 # Each deal has a monetary value and must be placed in a Stage. Deals can be owned by a User, and followed by one or many Users.
 # Each Deal consists of standard data fields but can also contain a number of custom fields. The custom fields can be recognized by long hashes as keys.
 # These hashes can be mapped against DealField.key. The corresponding label for each such custom field can be obtained from DealField.name.
-class Deals
-  include HTTP
+class Deals < Base
 
-  def initialize
-    puts HTTP.get("https://api.pipedrive.com/v1/deals?start=0&api_token=c306e8dc8011d1f255b02aaed556ab1242800d79").to_s
+  def all
+    response = HTTP.get("#{API_URL}/deals", :params => {:api_token => @api_token})
+    response.parse
   end
 
 end
