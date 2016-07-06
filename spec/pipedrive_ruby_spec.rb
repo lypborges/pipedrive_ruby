27fbd8df3bb65b7d91ac95cfad001ea0aed5e92a
Felipe Borges Ferreira <lypborges@gmail.com>
2016-07-06 18:43:56 -0300

    fix ignore

--- spec/pipedrive_ruby_spec.rb
+++ spec/pipedrive_ruby_spec.rb
@@ -5,8 +5,5 @@ describe PipedriveRuby do
     it 'has a version number' do
       expect(PipedriveRuby::VERSION).not_to be_nil
     end
-    it 'has alpha' do
-      expect(PipedriveRuby::VERSION).to include("alpha")
-    end
   end
 end
